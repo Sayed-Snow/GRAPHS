2c42f615ff2c53d888020d57f47bc6eb3d359855
Sayed <80715156+Sayed-Snow@users.noreply.github.com>
2022-03-30 21:17:52 +0200

    Update

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import matplotlib.pyplot as plt
 
 response = requests.get("http://sam-user-activity.eu-west-1.elasticbeanstalk.com")
 
-
-un_date=[]
-un_users=[]
-f_date=[]
-f_users=[]
+un_date = []
+un_users = []
+f_date = []
+f_users = []
 users = json.loads(response.text)
 for i in users:
     un_date.append(i)
     un_users.append(users[i])
-
 
 begin = str(input("Do you wish to filter the data?(yes or no)"))
 
@@ -40,17 +38,14 @@ if begin.lower() == 'yes':
                 f_users.append(un_users[i])
                 i += 1
 
-
     plt.plot(f_date, f_users)
     plt.title(' Users Vs Year')
     plt.xlabel('Year')
     plt.ylabel('Users')
     plt.show()
 if begin.lower() == 'no':
-
     plt.plot(un_date, un_users)
     plt.title(' Users Vs Year')
     plt.xlabel('Year')
     plt.ylabel('Users')
     plt.show()
-
